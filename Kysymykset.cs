4a0232ec74d54f1d34076ee456069c5c110625a4
joonasronn <joonas.ronn@gmail.com>
2019-01-28 12:58:00 +0200

    asdasdasd

---
+++ Kysymykset.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace Kysymykset
+{
+    class Kysymykset
+    {
+        public int kierrokset = 1;
+        List<Kysymykset> luetutKysymykset;
+        Pelaaja p = new Pelaaja();
+        public string Kysymys { get; set; }
+        public string Kyllä { get; set; }
+
+        public string Ei { get; set; }
+        public string Vastaus { get; set; }
+
+        public Kysymykset()
+        {
+        }
+
+        public Kysymykset(string kysymys, string kyllä, string ei, string vastaus)
+        {
+            Kysymys = kysymys;
+            Kyllä = kyllä;
+            Ei = ei;
+            Vastaus = vastaus;
+
+        }
+
+        public void lueKysymykset()
+        {
+            StreamReader lukija = new StreamReader(@"c:\work\kysymykset2.txt", Encoding.GetEncoding("iso-8859-1"));
+
+            string rivi = lukija.ReadLine();
+            luetutKysymykset = new List<Kysymykset>();
+            while (rivi != null)
+            {
+                string[] vaihtoehdot = rivi.Split("|");
+                Kysymykset k = new Kysymykset();
+                k.Kysymys = vaihtoehdot[0];
+                k.Kyllä = vaihtoehdot[1];
+                k.Ei = vaihtoehdot[2];
+                k.Vastaus = vaihtoehdot[3];
+
+                luetutKysymykset.Add(k);
+                rivi = lukija.ReadLine();
+            }
+
+            lukija.Close();
+        }
+        public int kysyKysymys()
+        {
+            int kierrokset = 0;
+
+            int piste = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+
+            Console.Clear();
+            Console.WriteLine("Hei!");
+            Console.WriteLine($"{luetutKysymykset[i].Kysymys}");
+            Console.WriteLine($"1. {luetutKysymykset[i].Kyllä}");
+            Console.WriteLine($"2. {luetutKysymykset[i].Ei}");
+            Console.WriteLine("Mikä on vastaus?");
+            string check = Console.ReadLine();
+
+                if (check == "1")
+                {
+                    check = luetutKysymykset[i].Kyllä;
+                }
+                if (check == "2")
+                {
+                    check = luetutKysymykset[i].Ei;
+                }
+
+            if (check == luetutKysymykset[i].Vastaus)
+            {
+                Console.WriteLine("Oikein!");
+                    //p.Pistelisäys();
+                    piste++;
+                        kierrokset++;
+                
+            }
+            else
+                Console.WriteLine("Väärin!");
+                kierrokset++;
+       
+            }
+            return piste;
+        }
+        
+
+
+     
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Kysymykset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu;
+            do
+            {
+            ListanLatausJaSekoitus();
+
+            string pelaajanNimi;
+            Kysymykset kys = new Kysymykset();
+            Pelaaja print = new Pelaaja();
+                Console.Clear();
+            Console.WriteLine("Hei, kerro nimesi!");
+            pelaajanNimi = Console.ReadLine();
+                Console.Clear();
+
+            kys.lueKysymykset();
+            int kokonaispisteet = 0;
+            
+
+            kokonaispisteet = kys.kysyKysymys();
+            Console.Clear();
+            Console.WriteLine($"Kiitos pelaamisesta {pelaajanNimi}!");
+            Console.WriteLine($"Sait {kokonaispisteet} / 10 oikein!");
+            Console.WriteLine("Valikko");
+            Console.WriteLine("1. Pelaa peli");
+            Console.WriteLine("2. Lopeta peli");
+            menu = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("KIITTIMOI!");
+
+
+            } while (menu != "2");
+
+            void ListanLatausJaSekoitus()
+            {
+                List<string> järjestyksessä;
+                using (StreamReader lataus = new StreamReader("c://work//temp//kysymykset.txt", Encoding.GetEncoding("iso-8859-1")))
+                {
+                    järjestyksessä = new List<string>();
+                    string rivi = lataus.ReadLine();
+                    while (rivi != null)
+                    {
+                        järjestyksessä.Add(rivi);
+                        rivi = lataus.ReadLine();
+                    }
+                }
+                Random rng = new Random();
+                string[] randomiksi = järjestyksessä.OrderBy(x => rng.Next()).ToArray();
+                using (StreamWriter tallennus = new StreamWriter("c://work//kysymykset2.txt", false, Encoding.GetEncoding("iso-8859-1")))
+                {
+                    foreach (var r in randomiksi)
+                    {
+                        tallennus.WriteLine(r);
+                    }
+                }
+                return;
+            }
+        }
+    }
+}
